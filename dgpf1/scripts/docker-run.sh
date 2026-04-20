f092962d1d85ea4293760637307bbaaad5829854
Steve Turoscy <sturoscy@globus.org>
2026-04-20 09:33:41 -0400

    Removing local containers

--- dgpf1/scripts/docker-run.sh
+++
@@ -1,7 +0,0 @@
-docker run --network=dgpf1_backend \
-    -e GUNICORN_BIND=0.0.0.0:8000 \
-    -e RUN_MIGRATIONS=true \
-    -e RUN_COLLECTSTATIC=true \
-    -v ${PWD}:/app \
-    -p 80:8000 \
-    -it operations-search-dgpf:latest
